2a6424599dcebea3957a90a1cf900778d926a4d8
Graeme Foster <graemefoster@microsoft.com>
2023-10-27 17:32:31 +0800

    Empty appsettings

--- AICentralTests/the_random_endpoint_picker.cs
+++ AICentralTests/the_random_endpoint_picker.cs
@@ -1,8 +1,5 @@
 using System.Net;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Text;
-using AICentral.Pipelines.EndpointSelectors.Random;
 using Newtonsoft.Json;
 using Shouldly;
 
