7c50b2b8190477a6098f43ef97a44ee55e58bc77
Graeme Foster <graemefoster@microsoft.com>
2023-12-07 11:45:35 +0800

    Merged

--- AICentral/Steps/EndpointSelectors/Priority/PriorityEndpointSelector.cs
+++ AICentral/Steps/EndpointSelectors/Priority/PriorityEndpointSelector.cs
@@ -35,8 +35,7 @@ public class PriorityEndpointSelector : IEndpointSelector
             try
             {
                 logger.LogWarning(e, "Prioritised Endpoint selector failed with primary. Trying fallback servers");
-                return await Handle(context, aiCallInformation, cancellationToken, _fallbackOpenAiEndpoints, true);
-                    isLastChance);
+                return await Handle(context, aiCallInformation, cancellationToken, _fallbackOpenAIEndpoints, isLastChance);
             }
             catch (HttpRequestException ex)
             {
