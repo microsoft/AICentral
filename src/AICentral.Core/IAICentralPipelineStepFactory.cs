b2b63f39029e2669693a3392b9a53c983bf00cb3
Graeme Foster <graemefoster@microsoft.com>
2024-01-29 18:46:16 +0800

    Added IServiceProvider for better use of Asp.Net DI container when building your steps

--- src/AICentral.Core/IAICentralPipelineStepFactory.cs
+++ src/AICentral.Core/IAICentralPipelineStepFactory.cs
@@ -6,10 +6,24 @@
 /// <typeparam name="T"></typeparam>
 public interface IAICentralPipelineStepFactory<out T> where T: IAICentralPipelineStep
 {
+    /// <summary>
+    /// The name AICentral looks for in your configuration to trigger this step.
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
     static virtual string ConfigName  => throw new NotImplementedException();
 
+    /// <summary>
+    /// Register any dependencies you need here. You may need to do this if you want to use the IServiceProvider to build your steps or their dependencies, later on. 
+    /// </summary>
+    /// <returns></returns>
     void RegisterServices(IServiceCollection services);
 
-    T Build();
+    /// <summary>
+    /// When a pipeline is executed you need to provide an instance of your step. If you need to store state within an execution then
+    /// you should provide a new instance of your Step every time. You may use the standard Asp.Net IServiceProvider to build your step if it helps.
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    /// <returns></returns>
+    T Build(IServiceProvider serviceProvider);
 
 }
