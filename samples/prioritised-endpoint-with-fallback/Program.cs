c1c33117207bbc3c266dafef28429150279a4e4a
Graeme Foster <graemefoster@microsoft.com>
2024-02-01 13:42:53 +0800

    REmoved baggage from quick sample

--- samples/prioritised-endpoint-with-fallback/Program.cs
+++ samples/prioritised-endpoint-with-fallback/Program.cs
@@ -1,23 +1,10 @@
 using AICentral.Configuration;
 using AICentral.Logging.AzureMonitor.AzureMonitorLogging;
-using Serilog;
-using Serilog.Events;
-using Serilog.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var logger = new LoggerConfiguration()
-    .MinimumLevel.Verbose()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-    .WriteTo
-    .Console()
-    .CreateLogger();
-
-builder.Host.UseSerilog(logger);
-
 builder.Services.AddAICentral(
     builder.Configuration,
-    startupLogger: new SerilogLoggerProvider(logger).CreateLogger("AICentralStartup"),
     additionalComponentAssemblies:
     [
         typeof(AzureMonitorLoggerFactory).Assembly,
@@ -28,10 +15,3 @@ var app = builder.Build();
 app.UseAICentral();
 
 app.Run();
-
-namespace AICentralWeb
-{
-    public partial class Program
-    {
-    }
-}
