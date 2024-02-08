5ace6b5cb4e2c7aecca20a16c42df288200a6d3f
Graeme Foster <graemefoster@microsoft.com>
2024-02-08 09:11:09 +0800

    Explicit preparation of requests. This will support more explicit remapping

--- src/AICentral/AzureOpenAIDetector.cs
+++ src/AICentral/AzureOpenAIDetector.cs
@@ -8,45 +8,101 @@ public class AzureOpenAIDetector
 {
     public async Task<IncomingCallDetails> Detect(string pipelineName, string deploymentName, AICallType callType, HttpRequest request, CancellationToken cancellationToken)
     {
-        if (request.ContentType?.Contains("json", StringComparison.InvariantCultureIgnoreCase) ?? false)
+        return callType switch
         {
-            //Pull out the text
-            var requestContent = await JsonDocument.ParseAsync(request.Body, cancellationToken: cancellationToken);
+            AICallType.Chat => await DetectChat(pipelineName, deploymentName, request, cancellationToken),
+            AICallType.Completions => await DetectCompletions(pipelineName, deploymentName, request, cancellationToken),
+            AICallType.Embeddings => await DetectEmbeddings(pipelineName, deploymentName, request, cancellationToken),
+            AICallType.Transcription => await DetectTranscription(pipelineName, deploymentName, request, cancellationToken),
+            AICallType.Translation => await DetectTranslation(pipelineName, deploymentName, request, cancellationToken),
+            AICallType.DALLE2 => await DetectDalle2(pipelineName, request, cancellationToken),
+            AICallType.DALLE3 => await DetectDalle3(pipelineName, deploymentName, request, cancellationToken),
+            _ => new IncomingCallDetails(pipelineName, callType, null, null, null, QueryHelpers.ParseQuery(request.QueryString.Value))
+        };
+    }
 
-            var promptText = callType switch
-            {
-                AICallType.Chat => string.Join(
-                    '\n',
-                    requestContent.RootElement.GetProperty("messages").EnumerateArray().Select(x => x.GetProperty("content").GetString())),
-                AICallType.Embeddings => requestContent.RootElement.GetProperty("input").GetString() ?? string.Empty,
-                AICallType.DALLE2 => requestContent.RootElement.GetProperty("prompt").GetString() ?? string.Empty,
-                AICallType.DALLE3 => requestContent.RootElement.GetProperty("prompt").GetString() ?? string.Empty,
-                AICallType.Completions => string.Join('\n', requestContent.RootElement.GetProperty("prompt").EnumerateArray().Select(x => x.GetString())),
-                _ => throw new ArgumentOutOfRangeException()
-            };
+    private async Task<IncomingCallDetails> DetectChat(string pipelineName, string deploymentName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        var requestContent = await JsonDocument.ParseAsync(request.Body, cancellationToken: cancellationToken);
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.Chat,
+            string.Join(
+                '\n',
+                requestContent.RootElement.GetProperty("messages").EnumerateArray()
+                    .Select(x => x.GetProperty("content").GetString())),
+            deploymentName,
+            requestContent,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
+    }
 
-            return new IncomingCallDetails(
-                pipelineName,
-                callType,
-                promptText,
-                deploymentName,
-                requestContent,
-                QueryHelpers.ParseQuery(request.QueryString.Value));
-        }
+    private async Task<IncomingCallDetails> DetectCompletions(string pipelineName, string deploymentName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        var requestContent = await JsonDocument.ParseAsync(request.Body, cancellationToken: cancellationToken);
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.Completions,
+            string.Join('\n', requestContent.RootElement.GetProperty("prompt").EnumerateArray().Select(x => x.GetString())),
+            deploymentName,
+            requestContent,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
+    }
 
-        if (callType == AICallType.Transcription || callType == AICallType.Translation)
-        {
-            var model = request.Form["model"];
-            return new IncomingCallDetails(pipelineName, callType, null, model, null,
-                QueryHelpers.ParseQuery(request.QueryString.Value));
-        }
+    private async Task<IncomingCallDetails> DetectEmbeddings(string pipelineName, string deploymentName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        var requestContent = await JsonDocument.ParseAsync(request.Body, cancellationToken: cancellationToken);
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.Embeddings,
+            requestContent.RootElement.GetProperty("input").GetString() ?? string.Empty,
+            deploymentName,
+            requestContent,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
+    }
 
-        if (request.Method == "GET")
-        {
-            return new IncomingCallDetails(pipelineName, callType, null, null, null,
-                QueryHelpers.ParseQuery(request.QueryString.Value));
-        }
+    private async Task<IncomingCallDetails> DetectTranscription(string pipelineName, string deploymentName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.Transcription,
+            null,
+            deploymentName,
+            null,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
+    }
 
-        throw new NotSupportedException("Call Type not supported by AI Central");
+    private async Task<IncomingCallDetails> DetectTranslation(string pipelineName, string deploymentName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.Translation,
+            null,
+            deploymentName,
+            null,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
+    }
+    
+    private async Task<IncomingCallDetails> DetectDalle2(string pipelineName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        var requestContent = await JsonDocument.ParseAsync(request.Body, cancellationToken: cancellationToken);
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.DALLE2,
+            null,
+            null,
+            requestContent,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
+    }
+    
+    private async Task<IncomingCallDetails> DetectDalle3(string pipelineName, string deploymentName, HttpRequest request, CancellationToken cancellationToken)
+    {
+        var requestContent = await JsonDocument.ParseAsync(request.Body, cancellationToken: cancellationToken);
+        return new IncomingCallDetails(
+            pipelineName,
+            AICallType.DALLE3,
+            null,
+            deploymentName,
+            requestContent,
+            QueryHelpers.ParseQuery(request.QueryString.Value));
     }
 }
