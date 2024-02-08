b2d94bb080c8297f3caead148d7e2a4de0b619d3
Graeme Foster <graemefoster@microsoft.com>
2024-02-08 15:05:10 +0800

    Adding test +semver:minor

---
+++ src/AICentralTests/Assistants/open_ai_assistants.cs
@@ -0,0 +1,26 @@
+﻿using AICentralTests.TestHelpers;
+using AICentralWeb;
+using Xunit.Abstractions;
+
+namespace AICentralTests.Assistants;
+
+public class open_ai_assistants
+{
+    private readonly TestWebApplicationFactory<Program> _factory;
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly HttpClient _httpClient;
+
+    public open_ai_assistants(TestWebApplicationFactory<Program> factory, ITestOutputHelper testOutputHelper)
+    {
+        _factory = factory;
+        _testOutputHelper = testOutputHelper;
+        factory.OutputHelper = testOutputHelper;
+        _httpClient = factory.CreateClient();
+    }
+
+    public async Task can_be_mapped_to_allow_load_balancing()
+    {
+        
+    }
+
+}
