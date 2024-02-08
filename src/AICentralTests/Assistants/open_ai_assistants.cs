e6a1a4b1e53b76aacf71e4ca838218b56b65dc46
Graeme Foster <graemefoster@microsoft.com>
2024-02-08 15:12:50 +0800

    Waiting for the open-ai client to add support for assistants before putting tests in

--- src/AICentralTests/Assistants/open_ai_assistants.cs
+++ src/AICentralTests/Assistants/open_ai_assistants.cs
@@ -1,5 +1,8 @@
 ﻿using AICentralTests.TestHelpers;
 using AICentralWeb;
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.Core.Pipeline;
 using Xunit.Abstractions;
 
 namespace AICentralTests.Assistants;
@@ -20,7 +23,13 @@ public class open_ai_assistants
 
     public async Task can_be_mapped_to_allow_load_balancing()
     {
-        
+        var client = new OpenAIClient(
+            new Uri("http://azure-to-azure-openai.localtest.me"),
+            new AzureKeyCredential("ignore"),
+            new OpenAIClientOptions(OpenAIClientOptions.ServiceVersion.V2023_12_01_Preview)
+            {
+                Transport = new HttpClientTransport(_httpClient)
+            });
     }
 
 }
