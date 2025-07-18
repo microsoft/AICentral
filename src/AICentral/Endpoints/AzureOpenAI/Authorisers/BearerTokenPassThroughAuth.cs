2285708f4b825f64bae782b7b88bf14fd8836a46
Graeme Foster <graemefoster@gmail.com>
2025-07-18 10:48:01 +0800

    Work with ai-foundry agents

--- src/AICentral/Endpoints/AzureOpenAI/Authorisers/BearerTokenPassThroughAuth.cs
+++ src/AICentral/Endpoints/AzureOpenAI/Authorisers/BearerTokenPassThroughAuth.cs
@@ -8,18 +8,40 @@ public class BearerTokenPassThroughAuth : IEndpointAuthorisationHandler
     public virtual Task ApplyAuthorisationToRequest(IRequestContext incomingRequest,
         HttpRequestMessage outgoingRequest)
     {
-        var authHeader = incomingRequest.RequestHeaders.Authorization.FirstOrDefault();
+        var authHeader = incomingRequest.RequestHeaders.Authorization.FirstOrDefault() ?? string.Empty;
+        
+        //TODO - add a config section to opt-in to this. Don't want to change existing behaviour.
+        var apiKeyHeader = incomingRequest.RequestHeaders["api-key"].FirstOrDefault();
 
-        authHeader = string.IsNullOrWhiteSpace(authHeader)
+        authHeader = string.IsNullOrWhiteSpace(authHeader) && string.IsNullOrWhiteSpace(apiKeyHeader)
             ? throw new ArgumentException("Bearer Token Pass Through. Could not find auth header on incoming request") : authHeader;
 
-        var parts = authHeader.Split("Bearer ");
-        if (parts.Length != 2)
+        if (authHeader.StartsWith("bearer", StringComparison.InvariantCultureIgnoreCase))
         {
-            throw new ArgumentException("Bearer Token Pass Through. Unexpected Authorisation scheme on incoming request");
+            var parts = authHeader.Split(" ");
+            if (parts.Length != 2)
+            {
+                //fallback?
+                if (!string.IsNullOrWhiteSpace(apiKeyHeader))
+                {
+                    outgoingRequest.Headers.TryAddWithoutValidation("api-key", apiKeyHeader);
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        "Bearer Token Pass Through. Unexpected Authorisation scheme on incoming request");
+                }
+            }
+            else
+            {
+                outgoingRequest.Headers.Authorization = new AuthenticationHeaderValue(parts[0], parts[1]);
+            }
         }
 
-        outgoingRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", parts[1]);
+        if (!string.IsNullOrWhiteSpace(apiKeyHeader))
+        {
+            outgoingRequest.Headers.Add("api-key", apiKeyHeader);
+        }
 
         return Task.CompletedTask;
     }
